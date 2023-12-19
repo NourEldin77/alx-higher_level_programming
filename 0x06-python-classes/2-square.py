2f592158bf10a8e7b8502f0f15ef613826662a92
NourEldin77 <noureldean.2003@gmail.com>
2023-12-19 23:08:27 +0200

    square_private_exception

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" square class """
+
+
+class Square:
+
+    """ simple square
+    Atrributes:
+        size: size of square
+    """
+    def __init__(self, size=0):
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
