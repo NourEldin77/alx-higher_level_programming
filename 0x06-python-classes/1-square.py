3a1a4528a278177c37388ac5a26bf7a2ded9986a
NourEldin77 <noureldean.2003@gmail.com>
2023-12-19 22:11:16 +0200

    square_private_attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" square class """
+
+
+class Square:
+    """ simple square
+    Atrributes:
+        size: size of square
+    """
+    def __init__(self, size):
+        self._Square__size = size
