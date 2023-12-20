d486aae2beb5bca98892c7d947c3d440f9c0d4d5
NourEldin77 <noureldean.2003@gmail.com>
2023-12-21 00:04:28 +0200

    fix: safe_print_list_intergers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list_intergers(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     counter = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             counter += 1
-        except (ValueError, TypeError):
+        except (TypeError, ValueError):
             continue
     print()
     return counter
