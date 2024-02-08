8bff3c82d13f756f9a3665d7cc51ae63d7d1dbcc
NourEldin77 <noureldean.2003@gmail.com>
2024-02-08 03:29:15 +0200

    Task: student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" student class module """
+
+
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        """" instantiation object method """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return a dict representation """
+        if (type(attrs) is list and
+                all(type(ele) is str for ele in attrs)):
+            return {attr: getattr(self, attr) for attr in attrs if
+                    (hasattr(self, attr))}
+        return (self.__dict__)
