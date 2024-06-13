2a18096c7d858bf76041b24a214a91e0d1a4184a
NourEldin77 <noureldean.2003@gmail.com>
2024-06-13 03:05:04 +0300

    Task4,5: script.js

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+const header = $('header');
+$('div#toggle_header').on('click', () => {
+  if (header.hasClass('green')) {
+    header.removeClass('green');
+    header.addClass('red');
+  } else {
+    header.removeClass('red');
+    header.addClass('green');
+  }
+});
