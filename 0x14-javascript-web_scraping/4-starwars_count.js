da6ef2b6981c693c47a90348dd61d32e0242c2d2
NourEldin77 <noureldean.2003@gmail.com>
2024-07-31 01:53:49 +0300

    Fix: 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,11 +5,11 @@ const URL = process.argv[2];
 const actorID = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(URL, (err, res, body) => {
-  let count = 0;
   if (err) throw err;
+  let count = 0;
   const films = JSON.parse(body).results;
-  for (const ele of films) {
-    if (ele.characters.includes(actorID)) {
+  for (const film of films) {
+    if (film.characters.includes(actorID)) {
       count++;
     }
   }
